79b5226beb5391a676b4b8f4622d387c668a9620
francois <francois.chabot.dev@gmail.com>
2018-08-28 14:18:01 -0400

    added minimal testing to quaternion

--- include/vecpp/quat.h
+++ include/vecpp/quat.h
@@ -2,6 +2,8 @@
 #define VECPP_QUAT_H_INCLUDED
 
 #include "vecpp/config.h"
+
+#include "vecpp/angle.h"
 #include "vecpp/vec.h"
 
 #include <array>
@@ -10,38 +12,38 @@
 
 namespace VECPP_NAMESPACE {
 
-// We could technically provide sane-ish defaults for most functions,
 template <typename T>
-struct Quat { 
+struct Quat {
   using value_type = T;
 
-  constexpr std::size_t size() const { return 4; }
-
-  constexpr T& operator[](std::size_t i) { return data_[i]; }
-  constexpr const T& operator[](std::size_t i) const { return data_[i]; }
-
-  constexpr T* data() { return data_; }
-  constexpr const T* data() const { return data_; }
-
-  static constexpr Quat angle_axis(const T& angle, const Vec<T, 3>& axis) {
-    const T s = std::sin(angle * T(0.5));
-
-    return {std::cos(angle * T(0.5)), axis[0] * s, axis[1] * s, axis[2] * s};
-  }
+  static constexpr Quat angle_axis(const Angle<T>& angle,
+                                   const Vec<T, 3>& axis);
 
   // Left public for aggregate initialization.
-  T data_[4]; //order is implicitely w x y z
+  T w;
+  T x;
+  T y;
+  T z;
 };
+
+template <typename T>
+constexpr Quat<T> Quat<T>::angle_axis(const Angle<T>& angle,
+                                      const Vec<T, 3>& axis) {
+  const T s = std::sin(angle.as_rad() * T(0.5));
+  const T c = std::cos(angle.as_rad() * T(0.5));
+
+  return {c, axis[0] * s, axis[1] * s, axis[2] * s};
+}
 
 template <typename T>
 constexpr Quat<T>& operator*=(Quat<T>& lhs, const Quat<T>& rhs) {
   const Quat<T> p(lhs);
   const Quat<T> q(rhs);
 
-  lhs[0] = p[0] * q[0] - p[1] * q[1] - p[2] * q[2] - p[3] * q[3];
-  lhs[1] = p[0] * q[1] + p[1] * q[0] + p[2] * q[3] - p[3] * q[2];
-  lhs[2] = p[0] * q[2] + p[2] * q[0] + p[3] * q[1] - p[1] * q[3];
-  lhs[3] = p[0] * q[3] + p[3] * q[0] + p[1] * q[2] - p[2] * q[1];
+  lhs.w = p.w * q.w - p.x * q.x - p.y * q.y - p.z * q.z;
+  lhs.x = p.w * q.x + p.x * q.w + p.y * q.z - p.z * q.y;
+  lhs.y = p.w * q.y + p.y * q.w + p.z * q.x - p.x * q.z;
+  lhs.z = p.w * q.z + p.z * q.w + p.x * q.y - p.y * q.x;
 
   return lhs;
 }
@@ -56,11 +58,11 @@ constexpr Quat<T> operator*(const Quat<T>& lhs, const Quat<T>& rhs) {
 // Rotate a vector by the quaternion
 template <typename T>
 constexpr Vec<T, 3> operator*(const Quat<T>& lhs, const Vec<T, 3>& rhs) {
-  const Vec<T, 3> q_v = {lhs[1], lhs[2], lhs[3]};
+  const Vec<T, 3> q_v = {lhs.x, lhs.y, lhs.z};
   const Vec<T, 3> uv = cross(q_v, rhs);
   const Vec<T, 3> uuv = cross(q_v, uv);
 
-  return rhs + ((uv * lhs[0]) + uuv) * T(2);
+  return rhs + ((uv * lhs.w) + uuv) * T(2);
 }
 }
 #endif
