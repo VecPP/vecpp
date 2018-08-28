09f6df1bf64d443e5284f371840292c65b25116a
francois <francois.chabot.dev@gmail.com>
2018-08-28 13:41:11 -0400

    added angle type

---
+++ include/vecpp/angle.h
@@ -0,0 +1,220 @@
+#ifndef VECPP_ANGLE_H_INCLUDED
+#define VECPP_ANGLE_H_INCLUDED
+
+#include "vecpp/config.h"
+#include "vecpp/pi.h"
+
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <type_traits>
+
+namespace VECPP_NAMESPACE {
+
+// Angle always represents value within the ]-PI,PI] radians range.
+template <typename T>
+class Angle {
+  static_assert(std::is_floating_point<T>::value,
+                "Only Angles of floating point types are allowed");
+
+ public:
+  using value_type = T;
+
+  static constexpr Angle from_rad(const value_type&);
+  static constexpr Angle from_deg(const value_type&);
+
+  // The argument MUST be in the ]-PI, PI] range.
+  static constexpr Angle from_constrained_rad(const value_type&);
+
+  // The argument MUST be in the ]-180, 180] range.
+  static constexpr Angle from_constrained_deg(const value_type&);
+
+  constexpr value_type as_deg() const;
+  constexpr value_type as_rad() const;
+
+ private:
+  value_type value_;
+
+  // Constructs an angle from a constrained radian value.
+  explicit constexpr Angle(const T&);
+};
+
+template <typename T>
+constexpr Angle<T> operator-(const Angle<T>& rhs) {
+  T value = rhs.as_rad();
+
+  // Special case, we keep positive pi.
+  if (value != pi<T>) {
+    value = -value;
+  }
+
+  return Angle<T>::from_constrained_rad(value);
+}
+
+template <typename T>
+constexpr Angle<T>& operator+=(Angle<T>& lhs, const Angle<T>& rhs) {
+  T val = lhs.as_rad() + rhs.as_rad();
+
+  // Since both lhs and rhs are in the ]-PI,PI] range, the sum is in the
+  // ]-2PI-1,2PI] range, so we can make assumptions in the constraining process.
+  if (val > pi<T>) {
+    val -= two_pi<T>;
+  } else if (val <= -pi<T>) {
+    val += two_pi<T>;
+  }
+
+  lhs = Angle<T>::from_constrained_rad(val);
+
+  return lhs;
+}
+
+template <typename T>
+constexpr Angle<T> operator+(const Angle<T>& lhs, const Angle<T>& rhs) {
+  auto result = lhs;
+  result += rhs;
+  return result;
+}
+
+template <typename T>
+constexpr Angle<T>& operator-=(Angle<T>& lhs, const Angle<T>& rhs) {
+  T val = lhs.as_rad() - rhs.as_rad();
+
+  // Since both lhs and rhs are in the ]-PI,PI] range, the difference is in the
+  // ]-2PI,2PI[ range, so we can make assumptions in the constraining process.
+  if (val > pi<T>) {
+    val -= two_pi<T>;
+  } else if (val <= -pi<T>) {
+    val += two_pi<T>;
+  }
+
+  lhs = Angle<T>::from_constrained_rad(val);
+
+  return lhs;
+}
+
+template <typename T>
+constexpr Angle<T> operator-(const Angle<T>& lhs, const Angle<T>& rhs) {
+  auto result = lhs;
+  result -= rhs;
+  return result;
+}
+
+template <typename T>
+constexpr Angle<T>& operator*=(Angle<T>& lhs, const T& rhs) {
+  lhs = Angle<T>::from_rad(lhs.as_rad() * rhs);
+  return lhs;
+}
+
+template <typename T>
+constexpr Angle<T> operator*(const Angle<T>& lhs, const T& rhs) {
+  auto result = lhs;
+  result *= rhs;
+  return result;
+}
+
+template <typename T>
+constexpr Angle<T> operator*(const T& lhs, const Angle<T>& rhs) {
+  return rhs * lhs;
+}
+
+template <typename T>
+constexpr Angle<T>& operator/=(Angle<T>& lhs, const T& rhs) {
+  lhs = Angle<T>::from_rad(lhs.as_rad() / rhs);
+  return lhs;
+}
+
+template <typename T>
+constexpr Angle<T> operator/(const Angle<T>& lhs, const T& rhs) {
+  auto result = lhs;
+  result /= rhs;
+  return result;
+}
+
+// COMPARISONS
+template <typename T>
+constexpr bool operator==(const Angle<T>& lhs, const Angle<T>& rhs) {
+  return lhs.as_rad() == rhs.as_rad();
+}
+
+template <typename T>
+constexpr bool operator!=(const Angle<T>& lhs, const Angle<T>& rhs) {
+  return lhs.as_rad() != rhs.as_rad();
+}
+
+template <typename T>
+constexpr bool operator<(const Angle<T>& lhs, const Angle<T>& rhs) {
+  return lhs.as_rad() < rhs.as_rad();
+}
+
+template <typename T>
+constexpr bool operator>(const Angle<T>& lhs, const Angle<T>& rhs) {
+  return lhs.as_rad() > rhs.as_rad();
+}
+
+template <typename T>
+constexpr bool operator<=(const Angle<T>& lhs, const Angle<T>& rhs) {
+  return lhs.as_rad() <= rhs.as_rad();
+}
+
+template <typename T>
+constexpr bool operator>=(const Angle<T>& lhs, const Angle<T>& rhs) {
+  return lhs.as_rad() >= rhs.as_rad();
+}
+
+// IOSTREAM
+template <typename T>
+std::ostream& operator<<(std::ostream& stream, const Angle<T>& v) {
+  return stream << v.as_deg() << "°";
+}
+
+// CONSTRUCTION
+template <typename T>
+constexpr Angle<T>::Angle(const T& v) : value_(v) {}
+
+template <typename T>
+constexpr Angle<T> Angle<T>::from_constrained_rad(const T& v) {
+  assert(v > -pi<float> && v <= pi<float>);
+
+  return Angle<T>(v);
+}
+
+template <typename T>
+constexpr Angle<T> Angle<T>::from_constrained_deg(const T& v) {
+  return from_constrained_rad(v / T(180) * pi<T>);
+}
+
+template <typename T>
+constexpr Angle<T> Angle<T>::from_rad(const T& v) {
+  // Unfortunately, std::fmod is not constexpr, so we have to roll our own...
+  T constrained = v + pi<T>;
+
+  T div = std::floor(constrained / two_pi<T>);
+  constrained -= div * two_pi<T>;
+
+  if (constrained <= T(0)) {
+    constrained += two_pi<T>;
+  }
+
+  constrained -= pi<T>;
+
+  return from_constrained_rad(constrained);
+}
+
+template <typename T>
+constexpr Angle<T> Angle<T>::from_deg(const T& v) {
+  return from_rad(v / T(180) * pi<T>);
+}
+
+// CONVERSION
+template <typename T>
+constexpr T Angle<T>::as_deg() const {
+  return value_ * T(180) / pi<T>;
+}
+
+template <typename T>
+constexpr T Angle<T>::as_rad() const {
+  return value_;
+}
+}
+
+#endif
---
+++ tests/angle.cpp
@@ -0,0 +1,177 @@
+#include "catch.hpp"
+
+#include "vecpp/vecpp.h"
+
+namespace angle_literals_float {
+constexpr vecpp::Angle<float> operator""_deg(long double v) {
+  return vecpp::Angle<float>::from_deg(float(v));
+}
+
+constexpr vecpp::Angle<float> operator""_rad(long double v) {
+  return vecpp::Angle<float>::from_rad(float(v));
+}
+}
+
+namespace angle_literals_double {
+constexpr vecpp::Angle<double> operator""_deg(long double v) {
+  return vecpp::Angle<double>::from_deg(double(v));
+}
+
+constexpr vecpp::Angle<double> operator""_rad(long double v) {
+  return vecpp::Angle<double>::from_rad(double(v));
+}
+}
+
+using vecpp::pi;
+using Catch::Matchers::WithinAbs;
+
+using Angle = vecpp::Angle<float>;
+
+TEST_CASE("create from radians or degrees", "[angle]") {
+  using namespace angle_literals_float;
+
+  auto a = 0.0_rad;
+  auto b = 0.0_deg;
+
+  static_assert(std::is_same_v<decltype(a)::value_type, float>);
+  static_assert(sizeof(a) == sizeof(float));
+
+  auto c = Angle::from_rad(pi<float>);
+  auto d = 180.0_deg;
+
+  REQUIRE(a == b);
+  REQUIRE(c == d);
+  REQUIRE(a != c);
+  REQUIRE(a != d);
+}
+
+TEST_CASE("create from double", "[angle]") {
+  using namespace angle_literals_double;
+
+  auto a = 0.0_rad;
+  static_assert(std::is_same_v<decltype(a)::value_type, double>);
+  static_assert(sizeof(a) == sizeof(double));
+}
+
+TEST_CASE("copy_and_reassign", "[angle]") {
+  using namespace angle_literals_float;
+  auto a = 10.0_deg;
+
+  auto b = a;
+  auto c = 20.0_deg;
+
+  c = a;
+
+  REQUIRE(b == a);
+  REQUIRE(c == a);
+}
+
+TEST_CASE("simple constraining", "[angle]") {
+  using namespace angle_literals_float;
+  auto a = 90.0_deg;
+  auto b = 450.0_deg;
+  auto c = 810.0_deg;
+
+  REQUIRE_THAT(a.as_deg(), WithinAbs(90.0f, 0.0001f));
+  REQUIRE_THAT(b.as_deg(), WithinAbs(90.0f, 0.0001f));
+  REQUIRE_THAT(c.as_deg(), WithinAbs(90.0f, 0.0001f));
+}
+
+TEST_CASE("negative constraining", "[angle]") {
+  using namespace angle_literals_float;
+  auto a = -90.0_deg;
+  auto b = -180.0_deg;
+  auto c = -200.0_deg;
+  auto d = -375.0_deg;
+
+  REQUIRE_THAT(a.as_deg(), WithinAbs(-90.0f, 0.0001f));
+  REQUIRE_THAT(b.as_deg(), WithinAbs(180.0f, 0.0001f));
+  REQUIRE_THAT(c.as_deg(), WithinAbs(160.0f, 0.0001f));
+  REQUIRE_THAT(d.as_deg(), WithinAbs(-15.0f, 0.0001f));
+}
+
+TEST_CASE("negate", "[angle]") {
+  auto a = -Angle::from_deg(0.0f);
+  auto b = -Angle::from_deg(10.0f);
+  auto c = -Angle::from_deg(-10.0f);
+  auto d = -Angle::from_deg(180.0f);
+
+  REQUIRE_THAT(a.as_deg(), WithinAbs(0.0f, 0.0001f));
+  REQUIRE_THAT(b.as_deg(), WithinAbs(-10.0f, 0.0001f));
+  REQUIRE_THAT(c.as_deg(), WithinAbs(10.0f, 0.0001f));
+  REQUIRE_THAT(d.as_deg(), WithinAbs(180.0f, 0.0001f));
+}
+
+TEST_CASE("add", "[angle]") {
+  using namespace angle_literals_float;
+
+  auto a = 0.0_deg + 10.0_deg;
+  auto b = 180.0_deg + 20.0_deg;
+  auto c = -10.0_deg + 30.0_deg;
+  auto d = 45.0_deg + 725.0_deg;
+
+  REQUIRE_THAT(a.as_deg(), WithinAbs(10.0f, 0.0001f));
+  REQUIRE_THAT(b.as_deg(), WithinAbs(-160.0f, 0.0001f));
+  REQUIRE_THAT(c.as_deg(), WithinAbs(20.0f, 0.0001f));
+  REQUIRE_THAT(d.as_deg(), WithinAbs(50.0f, 0.0001f));
+}
+
+TEST_CASE("sub", "[angle]") {
+  using namespace angle_literals_float;
+
+  auto a = 0.0_deg - 10.0_deg;
+  auto b = 180.0_deg - 20.0_deg;
+  auto c = -10.0_deg - 30.0_deg;
+  auto d = 45.0_deg - 725.0_deg;
+
+  REQUIRE_THAT(a.as_deg(), WithinAbs(-10.0f, 0.0001f));
+  REQUIRE_THAT(b.as_deg(), WithinAbs(160.0f, 0.0001f));
+  REQUIRE_THAT(c.as_deg(), WithinAbs(-40.0f, 0.0001f));
+  REQUIRE_THAT(d.as_deg(), WithinAbs(40.0f, 0.0001f));
+}
+
+TEST_CASE("scale_mul", "[angle]") {
+  using namespace angle_literals_float;
+
+  auto a = 0.0_deg * 10.0f;
+  auto b = 10.0_deg * 5.0f;
+  auto c = 90.0_deg * 3.0f;
+
+  REQUIRE_THAT(a.as_deg(), WithinAbs(0.0f, 0.0001f));
+  REQUIRE_THAT(b.as_deg(), WithinAbs(50.0f, 0.0001f));
+  REQUIRE_THAT(c.as_deg(), WithinAbs(-90.0f, 0.0001f));
+}
+
+TEST_CASE("scale_div", "[angle]") {
+  using namespace angle_literals_float;
+
+  auto a = 0.0_deg * 10.0f;
+  auto b = 10.0_deg * 5.0f;
+  auto c = 90.0_deg * 3.0f;
+
+  REQUIRE_THAT(a.as_deg(), WithinAbs(0.0f, 0.0001f));
+  REQUIRE_THAT(b.as_deg(), WithinAbs(50.0f, 0.0001f));
+  REQUIRE_THAT(c.as_deg(), WithinAbs(-90.0f, 0.0001f));
+}
+
+TEST_CASE("minmax", "[angle]") {
+  using namespace angle_literals_float;
+
+  auto a = 10.0_deg;
+  auto b = -10.0_deg;
+
+  auto c = std::min(a, b);
+  auto d = std::max(a, b);
+
+  REQUIRE_THAT(c.as_deg(), WithinAbs(-10.0f, 0.0001f));
+  REQUIRE_THAT(d.as_deg(), WithinAbs(10.0f, 0.0001f));
+}
+
+TEST_CASE("constexpr usage", "[angle]") {
+  using namespace angle_literals_float;
+
+  constexpr Angle a = 30.0_deg;
+  constexpr Angle b = 90.0_deg;
+
+  static_assert(std::abs((3.0f * a - b).as_deg()) < 0.0001f);
+}
