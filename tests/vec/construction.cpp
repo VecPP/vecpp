01ccf57750354b4aeb46128d55fcc6af53e145ce
francois <francois.chabot.dev@gmail.com>
2018-08-31 12:51:47 -0400

    vector constexpr tests now embedded with regular tests

--- tests/vec/construction.cpp
+++ tests/vec/construction.cpp
@@ -5,66 +5,107 @@
 #include <sstream>
 
 TEST_CASE("Default construction compiles without warning", "[vec_construct]") {
-  vecpp::Vec<float, 2> a;
-  (void)a;
+  using Vec = vecpp::Vec<float, 2>;
+
+  {
+    Vec a;
+    (void)a;
+  }
+
+  {
+    // This isn't supposed to workn in constexpr
+    // constexpr Vec a;
+    // (void)a;
+    }
 }
 
 TEST_CASE("Build vec2 from aggregate initialization", "[vec_construct]") {
-  vecpp::Vec<float, 2> a = {1.0f, 2.0f};
-  vecpp::Vec<float, 2> b{1.0f, 2.0f};
+  using Vec = vecpp::Vec<float, 2>;
+  
+  {
+    Vec a = {1.0f, 2.0f};
+    Vec b{1.0f, 2.0f};
 
-  REQUIRE(a[0] == 1.0f);
-  REQUIRE(a[1] == 2.0f);
+    REQUIRE(a[0] == 1.0f);
+    REQUIRE(a[1] == 2.0f);
 
-  REQUIRE(a == b);
-}
+    REQUIRE(a == b);
+  }
 
-TEST_CASE("Build vec3 from aggregate initialization", "[vec_construct]") {
-  vecpp::Vec<float, 3> a = {1.0f, 2.0f, 3.0f};
-  vecpp::Vec<float, 3> b{1.0f, 2.0f, 3.0f};
+  {
+    constexpr Vec a = {1.0f, 2.0f};
+    constexpr Vec b = {1.0f, 2.0f};
 
-  REQUIRE(a[0] == 1.0f);
-  REQUIRE(a[1] == 2.0f);
-  REQUIRE(a[2] == 3.0f);
-
-  REQUIRE(a == b);
-}
-
-TEST_CASE("Build vec4 from aggregate initialization", "[vec_construct]") {
-  vecpp::Vec<float, 4> a = {1.0f, 2.0f, 3.0f, 4.0f};
-  vecpp::Vec<float, 4> b{1.0f, 2.0f, 3.0f, 4.0f};
-
-  REQUIRE(a[0] == 1.0f);
-  REQUIRE(a[1] == 2.0f);
-  REQUIRE(a[2] == 3.0f);
-  REQUIRE(a[3] == 4.0f);
-
-  REQUIRE(a == b);
-}
-
-TEST_CASE("use vec in range-based for", "[vec_iterate]") {
-  vecpp::Vec<float, 4> a = {1.0f, 2.0f, 3.0f, 4.0f};
-
-  for (auto v : a) {
-    (void)v;
+    static_assert(a[0] == 1.0f);
+    static_assert(a[1] == 2.0f);
+    static_assert(a == b);
   }
 }
 
-TEST_CASE("use vec in const range-based for", "[vec_iterate]") {
-  const vecpp::Vec<float, 4> a = {1.0f, 2.0f, 3.0f, 4.0f};
+TEST_CASE("use vec in range-based for", "[vec_iterate]") {
+  using Vec = vecpp::Vec<float, 4>;
 
-  for (auto v : a) {
-    (void)v;
+  {
+    Vec a = {1.0f, 2.0f, 3.0f, 4.0f};
+
+    for (auto& v : a) {
+      v += 1.0f;
+    }
+
+    REQUIRE(a[0] == 1.0f + 1.0f);
+    REQUIRE(a[1] == 2.0f + 1.0f);
+    REQUIRE(a[2] == 3.0f + 1.0f);
+    REQUIRE(a[3] == 4.0f + 1.0f);
+  }
+
+  // No constexpr equivalent
+}
+
+TEST_CASE("use const vec in range-based for", "[vec_iterate]") {
+  using Vec = vecpp::Vec<float, 4>;
+
+  {
+    const Vec a = {1.0f, 2.0f, 3.0f, 4.0f};
+
+    for (auto v : a) {
+      (void)v;
+    }
+  }
+
+  {
+    constexpr Vec a = {1.0f, 2.0f, 3.0f, 4.0f};
+    constexpr float b = [](){
+      float total = 0.0f;
+      for (auto v : a) {
+        total += v;
+      }
+      return total;
+    }();
+    static_assert(b == (1.0f + 2.0f + 3.0f + 4.0f));
   }
 }
 
 
 TEST_CASE("Vec::at() performs bounds-checking", "[vec_access]") {
-  const vecpp::Vec<float, 4> a = {1.0f, 2.0f, 3.0f, 4.0f};
+  using Vec = vecpp::Vec<float, 4>;
+  {
+    const Vec a = {1.0f, 2.0f, 3.0f, 4.0f};
 
-  REQUIRE(a.at(0) == 1.0f);
-  REQUIRE_THROWS(a.at(4));
-  REQUIRE_THROWS(a.at(5));
+    REQUIRE(a.at(0) == 1.0f);
+    REQUIRE_THROWS(a.at(4));
+    REQUIRE_THROWS(a.at(5));
+  }
+
+  {
+    constexpr Vec a = {1.0f, 2.0f, 3.0f, 4.0f};
+
+    static_assert(a.at(0) == 1.0f);
+
+    // Intentionally does not compile:
+    // static_assert(a.at(5)); 
+
+    REQUIRE_THROWS(a.at(5));
+  }
 }
 
 
@@ -76,4 +117,6 @@ TEST_CASE("Outputs to iostream", "[vec_format]") {
   stream << a;
 
   REQUIRE(stream.str() == "(0, 1, 2, 3)");
+
+  // No constexpr equivalent
 }
