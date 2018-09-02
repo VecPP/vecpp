0d5b3e1f393cf1d257a0b00d75423288c86446e5
Francois Chabot <francois.chabot.dev@gmail.com>
2018-09-02 16:49:43 -0400

    Fixed typo in placeholder unary vec functions

--- include/vecpp/vec/operations_per_member.h
+++ include/vecpp/vec/operations_per_member.h
@@ -53,7 +53,7 @@ constexpr Vec<T, l, f> floor(const Vec<T, l, f>& v) {
 template <typename T, std::size_t l, Flags f>
 constexpr Vec<T, l, f> fract(const Vec<T, l, f>& v) {
   Vec<T, l, f> result = {0};
-  for (std::size_t i = 0; i < lhs.size(); ++i) {
+  for (std::size_t i = 0; i < v.size(); ++i) {
     result[i] = fract(v[i]);
   }
   return result;
@@ -63,7 +63,7 @@ constexpr Vec<T, l, f> fract(const Vec<T, l, f>& v) {
 template <typename T, std::size_t l, Flags f>
 constexpr Vec<T, l, f> round(const Vec<T, l, f>& v) {
   Vec<T, l, f> result = {0};
-  for (std::size_t i = 0; i < lhs.size(); ++i) {
+  for (std::size_t i = 0; i < v.size(); ++i) {
     result[i] = round(v[i]);
   }
   return result;
@@ -73,7 +73,7 @@ constexpr Vec<T, l, f> round(const Vec<T, l, f>& v) {
 template <typename T, std::size_t l, Flags f>
 constexpr Vec<T, l, f> sign(const Vec<T, l, f>& v) {
   Vec<T, l, f> result = {0};
-  for (std::size_t i = 0; i < lhs.size(); ++i) {
+  for (std::size_t i = 0; i < v.size(); ++i) {
     result[i] = sign(v[i]);
   }
   return result;
@@ -83,7 +83,7 @@ constexpr Vec<T, l, f> sign(const Vec<T, l, f>& v) {
 template <typename T, std::size_t l, Flags f>
 constexpr Vec<T, l, f> truc(const Vec<T, l, f>& v) {
   Vec<T, l, f> result = {0};
-  for (std::size_t i = 0; i < lhs.size(); ++i) {
+  for (std::size_t i = 0; i < v.size(); ++i) {
     result[i] = trunc(v[i]);
   }
   return result;
