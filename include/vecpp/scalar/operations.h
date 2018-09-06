6b7a75bd4dec2cc51f3195ddd7ea4c2a8faaec75
Francois Chabot <francois.chabot.dev@gmail.com>
2018-09-05 22:13:27 -0400

    Ceil() and floor() properly handle large values
    Fix #20

--- include/vecpp/scalar/operations.h
+++ include/vecpp/scalar/operations.h
@@ -102,6 +102,14 @@ constexpr T exp(const T& v) {
 
 template <typename T>
 constexpr T ceil(const T& v) {
+  
+  constexpr long long int range_max = 1LL << std::numeric_limits<T>::digits;
+  constexpr long long int range_min = -range_max;
+
+  if (v >= range_max || v <= range_min) {
+    return v;
+  }
+  
   long long int x = static_cast<long long int>(v);
   if (v == T(x) || v < T(0)) {
     return T(x);
@@ -111,6 +119,14 @@ constexpr T ceil(const T& v) {
 
 template <typename T>
 constexpr T floor(const T& v) {
+  
+  constexpr long long int range_max = 1LL << std::numeric_limits<T>::digits;
+  constexpr long long int range_min = -range_max;
+
+  if (v >= range_max || v <= range_min) {
+    return v;
+  }
+  
   long long int x = static_cast<long long int>(v);
   if (v == T(x) || v > T(0)) {
     return T(x);
--- tests/scalar.cpp
+++ tests/scalar.cpp
@@ -18,3 +18,14 @@ TEST_CASE("constexpr sqrt", "[scalar]") {
   constexpr float x = vecpp::sqrt<ctf>(16.0f);
   static_assert(x == 4.0f);
 }
+
+TEST_CASE("very large constexpr ceil", "[scalar][constexpr][ceil]") {
+  float ref_val = 10.0e20f;
+  float x = vecpp::ceil<ctf>(ref_val);
+
+  float ref_val_2 = -10.0e20f;
+  float y = vecpp::ceil<ctf>(ref_val_2);
+
+  REQUIRE(x == ref_val);
+  REQUIRE(y == ref_val_2);
+}
