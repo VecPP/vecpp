7f279f3b6071ea26b3194f4ebbe24667c43ad1ba
VecppCI <francois.chabot.dev+vecppci@gmail.com>
2018-09-08 20:18:11 +0000

    Automated deployment to GitHub Pages: a3bb96393a47b1eed7f8aef86e4757d3def3ca13

--- vecpp/vecpp_single.h
+++ vecpp/vecpp_single.h
@@ -259,6 +259,9 @@ namespace CSTE_MATH_NAMESPACE {
 namespace cosine_detail {
 constexpr long double recur_helper(long double r_2, long double num,
                                    long double fact, std::size_t i) {
+  if(i >= 160) {
+    return 0;
+  }
   fact *= (i - 1);
   fact *= (i);
   num *= r_2;
@@ -283,6 +286,9 @@ namespace CSTE_MATH_NAMESPACE {
 namespace sine_detail {
 constexpr long double recur_helper(long double r_2, long double num,
                                    double fact, std::size_t i) {
+  if(i >= 160) {
+    return 0;
+  }
   fact *= (i - 1);
   fact *= (i);
   num *= r_2;
